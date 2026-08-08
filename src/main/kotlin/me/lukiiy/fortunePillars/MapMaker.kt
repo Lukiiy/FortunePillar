f0770564c952453f49ec0092d0187067657e75b7
Lukiiy <lukiiy@proton.me>
2026-08-08 11:22:56 -0300

    Add genPillars

--- src/main/kotlin/me/lukiiy/fortunePillars/MapMaker.kt
+++ src/main/kotlin/me/lukiiy/fortunePillars/MapMaker.kt
@@ -3,6 +3,10 @@ package me.lukiiy.fortunePillars
 import org.bukkit.*
 import org.bukkit.generator.ChunkGenerator
 import java.util.*
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.floor
+import kotlin.math.sin
 
 class MapMaker {
     fun create(): World? {
@@ -10,8 +14,6 @@ class MapMaker {
 
         val world = creator.createWorld()?.apply {
             setGameRule(GameRules.SPAWN_MOBS, false)
-            setGameRule(GameRules.ADVANCE_TIME, false)
-            setGameRule(GameRules.RANDOM_TICK_SPEED, 0)
             setGameRule(GameRules.ADVANCE_WEATHER, false)
             setGameRule(GameRules.SPECTATORS_GENERATE_CHUNKS, false)
             isAutoSave = false
@@ -24,4 +26,38 @@ class MapMaker {
     }
 
     class VoidGen : ChunkGenerator()
+
+    companion object {
+        fun genPillars(center: Location, totalPoints: Int, initialRadius: Int): List<Location> {
+            val world = center.world ?: return emptyList()
+            val top = ArrayList<Location>(totalPoints)
+            var pointsRemaining = totalPoints
+            var currentIdx = 0
+            val pi2 = PI * 2
+
+            while (pointsRemaining > 0) {
+                val ringCapacity = 8 + (currentIdx * 4)
+                val ringPoints = minOf(pointsRemaining, ringCapacity)
+                val currentRadius = initialRadius + (currentIdx * 8) // ring spacing!
+                val angleStep = pi2 / ringPoints
+
+                repeat(ringPoints) {
+                    val angle = it * angleStep
+
+                    // calculate coords
+                    val x = (center.x + floor(currentRadius * cos(angle))).toInt()
+                    val z = (center.z + floor(currentRadius * sin(angle))).toInt()
+
+                    for (y in 64..96) world.getBlockAt(x, y, z).setType(Material.BEDROCK, false)
+
+                    top.add(Location(world, x + .5, 97.0, z + .5))
+                }
+
+                pointsRemaining -= ringPoints
+                currentIdx++
+            }
+
+            return top
+        }
+    }
 }
