bc359ac4118ce13c32ec660aef2b958f0de8ebe6
Lukiiy <lukiiy@proton.me>
2026-08-08 21:20:56 -0300

    replace entries with registry

--- src/main/kotlin/me/lukiiy/fortunePillars/Pool.kt
+++ src/main/kotlin/me/lukiiy/fortunePillars/Pool.kt
@@ -1,16 +1,17 @@
 package me.lukiiy.fortunePillars
 
 import org.bukkit.Material
+import org.bukkit.Registry
 import org.bukkit.inventory.ItemStack
 
 object Pool {
-    private lateinit var validMaterials: List<Material>
+    val validMaterials: List<Material> by lazy {
+        Registry.MATERIAL.filter { it.isItem && !it.isAir && it !in excluded }.toList()
+    }
 
     val excluded = setOf(Material.BEDROCK, Material.BARRIER, Material.LIGHT, Material.STRUCTURE_BLOCK, Material.STRUCTURE_VOID, Material.COMMAND_BLOCK, Material.CHAIN_COMMAND_BLOCK, Material.REPEATING_COMMAND_BLOCK, Material.COMMAND_BLOCK_MINECART, Material.JIGSAW, Material.DEBUG_STICK, Material.KNOWLEDGE_BOOK, Material.SPAWNER)
 
     fun init() {
-        validMaterials = Material.entries.filterNot { !it.isItem || it.isAir || it in excluded }
-
         FortunePillars.getInstance().logger.info("Setting up item pools...")
     }
 
