800e9fcd0b8b07398d5b83c0ada1dec3306ab282
Lukiiy <lukiiy@proton.me>
2026-08-11 13:11:11 -0300

    Add isInBoundsVertically & isInBoundsHorizontally to MapBounds

--- src/main/kotlin/me/lukiiy/fortunePillars/arena/MapBounds.kt
+++ src/main/kotlin/me/lukiiy/fortunePillars/arena/MapBounds.kt
@@ -4,5 +4,8 @@ import org.bukkit.Location
 import kotlin.math.absoluteValue
 
 data class MapBounds(val center: Location, val radius: Int, val maxY: Int) {
-    operator fun contains(location: Location): Boolean = (location.blockX - center.blockX).absoluteValue <= radius && (location.blockZ - center.blockZ).absoluteValue <= radius && location.blockY <= maxY
+    operator fun contains(location: Location): Boolean = isInBoundsVertically(location) && isInBoundsHorizontally(location)
+
+    fun isInBoundsVertically(location: Location): Boolean = location.blockY <= maxY
+    fun isInBoundsHorizontally(location: Location): Boolean = (location.blockX - center.blockX).absoluteValue <= radius && (location.blockZ - center.blockZ).absoluteValue <= radius
 }
