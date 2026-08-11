0a93d573a86172fecdb5d8d123cef79d6f2ea5b0
Lukiiy <lukiiy@proton.me>
2026-08-11 12:20:07 -0300

    remove init & add LIGHT, TEST_BLOCK & TEST_INSTANCE_BLOCK to excluded material list

--- src/main/kotlin/me/lukiiy/fortunePillars/Pool.kt
+++ src/main/kotlin/me/lukiiy/fortunePillars/Pool.kt
@@ -9,11 +9,7 @@ object Pool {
         Registry.MATERIAL.filter { it.isItem && !it.isAir && it !in excluded }.toList()
     }
 
-    val excluded = setOf(Material.BEDROCK, Material.BARRIER, Material.LIGHT, Material.STRUCTURE_BLOCK, Material.STRUCTURE_VOID, Material.COMMAND_BLOCK, Material.CHAIN_COMMAND_BLOCK, Material.REPEATING_COMMAND_BLOCK, Material.COMMAND_BLOCK_MINECART, Material.JIGSAW, Material.DEBUG_STICK, Material.KNOWLEDGE_BOOK, Material.SPAWNER)
-
-    fun init() {
-        FortunePillars.getInstance().logger.info("Setting up item pools...")
-    }
+    val excluded = setOf(Material.BEDROCK, Material.BARRIER, Material.LIGHT, Material.STRUCTURE_BLOCK, Material.STRUCTURE_VOID, Material.COMMAND_BLOCK, Material.CHAIN_COMMAND_BLOCK, Material.REPEATING_COMMAND_BLOCK, Material.COMMAND_BLOCK_MINECART, Material.JIGSAW, Material.DEBUG_STICK, Material.KNOWLEDGE_BOOK, Material.LIGHT, Material.TEST_BLOCK, Material.TEST_INSTANCE_BLOCK)
 
     fun nextItem(amount: Int = 1) = ItemStack.of(validMaterials.random(), amount)
 
